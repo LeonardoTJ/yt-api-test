522424abd28ef4e46e0bc2cdffc95419d9e4fac8
Leonardo Meléndez <leonardo.melendez@uabc.edu.mx>
2020-11-12 00:26:13 -0800

    Formato de solicitudes y salidas

--- src/ytstats.py
+++ src/ytstats.py
@@ -1,14 +1,13 @@
 import py_colors as pc
-#import pprint
+import pprint
 import json
 import html
 import requests
 import datetime
 import dateutil.parser
-from dateutil import tz
 
+from dateutil import tz
 from googleapiclient.discovery import build
-from tqdm import tqdm
 
 class YoutubeStats:
     def __init__(self, api_key):
@@ -16,24 +15,35 @@ class YoutubeStats:
         self.youtube = build('youtube', 'v3', developerKey=api_key)
 
     def print_stream_info(self, channel_id):
-        url = f'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId={channel_id}&type=video&eventType=upcoming&key={self.api_key}'
+        url = f"https://youtube.googleapis.com/youtube/v3/channels?part=statistics%2Csnippet&id={channel_id}&fields=items(snippet%2Ftitle%2Cstatistics%2FsubscriberCount)&key={self.api_key}"
         json_url = requests.get(url)
         data = json.loads(json_url.text)
 
-        if(len(data["items"]) == 0):
-            print(pc.CRED + pc.CBOLD + "Sin resultados" + pc.CEND)
+        channel_name = data["items"][0]["snippet"]["title"]
+        subscribers = data["items"][0]["statistics"]["subscriberCount"]
+        print("{boldTag}{blueTag}{name}{endTag}{blueTag} - {subs:,} suscriptores{endTag}".format(boldTag=pc.CBOLD,
+                                                                                                blueTag=pc.CBLUE,
+                                                                                                name=channel_name,
+                                                                                                subs=int(subscribers),
+                                                                                                endTag=pc.CEND))
+
+        url = f"https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId={channel_id}&eventType=upcoming&maxResults=5&type=video&fields=pageInfo%2Citems(id%2FvideoId%2Csnippet%2Ftitle%2C)&key={self.api_key}"
+        json_url = requests.get(url)
+        data = json.loads(json_url.text)
+
+        if data["pageInfo"]["totalResults"] == 0:
+            print('\t{:10}{}{}{}'.format(pc.CYELLOW, "No hay transmisiones agendadas", pc.CEND))
             return
 
-        channel_name = data["items"][0]["snippet"]["channelTitle"]
-        
         streams = {}
         for item in data["items"]:
             id = item["id"]["videoId"]
-            url = f"https://youtube.googleapis.com/youtube/v3/videos?part=liveStreamingDetails&id={id}&key={self.api_key}"
+            url = f"https://youtube.googleapis.com/youtube/v3/videos?part=liveStreamingDetails&id={id}&fields=items(liveStreamingDetails%2FscheduledStartTime)&key={self.api_key}"
             json_url = requests.get(url)
+            data = json.loads(json_url.text)
             
-            data = json.loads(json_url.text)["items"][0]["liveStreamingDetails"]["scheduledStartTime"]
-            scheduledDate = dateutil.parser.parse(data).astimezone(tz.tzlocal())
+            dateString = data["items"][0]["liveStreamingDetails"]["scheduledStartTime"]
+            scheduledDate = dateutil.parser.parse(dateString).astimezone(tz.tzlocal())
             diff = scheduledDate - scheduledDate.now(tz.tzlocal())
             remainingTime = divmod(diff.total_seconds(), 60)
             remainingHours = divmod(remainingTime[0], 60)
@@ -42,21 +52,12 @@ class YoutubeStats:
                 streams[id] = item["snippet"]
                 streams[id]["title"] = html.unescape(streams[id]["title"])
                 streams[id]["scheduledDate"] = scheduledDate.strftime("%c")
-                streams[id]["remainingTime"] = "en {} horas, {} minutos".format(str(remainingHours[0]), str(int(remainingHours[1])))
-        
-        print(pc.CBOLD + pc.CBLUE + channel_name + pc.CEND)
-        if not bool(streams):
-            print('\t{:10}{}{}{}'.format(pc.CRED, pc.CBOLD, "No hay transmisiones agendadas", pc.CEND))
-            return
-        
-        # pp = pprint.PrettyPrinter()
-        # pp.pprint(streams)
-        # return
+                streams[id]["remainingTime"] = "en {} horas, {} minutos".format(str(int(remainingHours[0])), str(int(remainingHours[1])))
 
         i = 0
         for video in streams:
             i += 1
-            print("{greenTag}{count:10}. {title:50} {endTag}[{id:<10}]{greenTag}" \
+            print("{greenTag}{count:10}. {title} {endTag}[{id:<10}]{greenTag}" \
                   "\n\t\t({date}, {yellowTag}{remaining}{endTag}{greenTag}){endTag}".format(greenTag=pc.CGREEN,
                                                                                                 count=i,
                                                                                                 title=streams[video]["title"],
